ce806aef84b166af27d19bb972fe24a2153bb2c4
Ngoc Vo <ngocvo@usf.edu>
2022-02-25 08:11:03 -0500

    Commit changes

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ namespace T4_TeamBelaProblemOnKattis_Group_
                 const int MIN_HAND = 1;
                 const int MAX_HAND = 100;
                 // Get the input of hand and suit value from the user
-                Console.Write("Please Enter the the number of hands and the value of suit: ");
+                // Console.Write("Please Enter the the number of hands and the value of suit: ");
                 // Declare the variale to get the input, which is shown in uppercase, from the user
                 string input = Console.ReadLine().ToUpper();
                 // Declare the variale for the space in the user input
@@ -72,7 +72,7 @@ namespace T4_TeamBelaProblemOnKattis_Group_
                         while (numberOfCard < totalCard)
                         {
                             // Get the card description from user
-                            Console.Write("Please Enter the Card Description: ");
+                            // Console.Write("Please Enter the Card Description: ");
                             // Declare variable to get the card description from the user
                             string inputCard = Console.ReadLine().ToUpper();
                             /*
